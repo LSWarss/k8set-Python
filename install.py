e246ad87508a5ce3c6b060a2ffdb3e4bb53c7d3b
LSWarss <ls.warss98@gmail.com>
2020-09-01 20:12:56 +0200

    Repaired alias removal in installation script Closes #5

--- install.py
+++ install.py
@@ -9,23 +9,21 @@ pattern = "k8set"
 if("zsh" in shell):
     with open(f"{home}/.zshrc", "r") as f:
         lines = f.readlines()
-    with open(f"{home}/.zshrc", "a") as f:
+    with open(f"{home}/.zshrc", "w") as f:
         for line in lines:
-            if "k8set" in line:
+            if "k8set" not in line:
                 f.write(line)
         f.truncate()
 elif("bash" in shell):
     with open(f"{home}/.bash_profile", "r") as f:
         lines = f.readlines()
-    with open(f"{home}/.bash_profile", "a") as f:
+    with open(f"{home}/.bash_profile", "w") as f:
         for line in lines:
-            if "k8set" in line:
+            if "k8set" not in line:
                 f.write(line)
         f.truncate()
 
-installation_str =f"alias k8set= python {current_folder}/k8set.py"
-
-
+installation_str =fr"alias k8set='python {current_folder}/k8set.py'"
 
 if("zsh" in shell):
     print(f"Alias will be installed now by adding {installation_str} to {home}/.zshrc")
