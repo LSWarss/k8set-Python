6475473312a9d014fab4a9cfabf5c5823e2aea78
LSWarss <ls.warss98@gmail.com>
2020-09-01 21:07:51 +0200

    Tweaking script to handle good paths

--- src/k8set.py
+++ src/k8set.py
@@ -1,7 +1,7 @@
 import pathlib, os, re, glob
 
+current_folder = pathlib.Path(__file__).parent.absolute()
 arrKubeconfigs = glob.glob('*.kubeconfig') #It looks in current directory for files with extension kubeconfig and creates list of them
-print(arrKubeconfigs)
 
 if (len(arrKubeconfigs) == 0):
     print("Current directory does not contains any kubeconfig files.")
@@ -10,19 +10,19 @@ else:
     for i in range(len(arrKubeconfigs)):
         print(f"{i}. {arrKubeconfigs[i]}")
 
-file_number = input("Please type here file number:")
-number_regex='^[0-9]+$'
+    file_number = input("Please type here file number:")
+    number_regex='^[0-9]+$'
 
-if (re.match(number_regex, file_number) == True):
-    print("Please enter only numbers here")
-elif(int(file_number) < 0 and int(file_number) > len(arrKubeconfigs)):
-    print(f"Please select file number from range 0 and {len(arrKubeconfigs)}")
-else:
-    selected_kubeconfig = arrKubeconfigs[int(file_number)]
+    if (re.match(number_regex, file_number) == True):
+        print("Please enter only numbers here")
+    elif(int(file_number) < 0 and int(file_number) > len(arrKubeconfigs)):
+        print(f"Please select file number from range 0 and {len(arrKubeconfigs)}")
+    else:
+        selected_kubeconfig = arrKubeconfigs[int(file_number)]
 
-    print(f"Following kubeconfig: {selected_kubeconfig} will be set as active one")
-    
-    os.environ["KUBECONFIG"] = str(selected_kubeconfig)
+        print(f"Following kubeconfig: {selected_kubeconfig} will be set as active one")
+        
+        os.environ["KUBECONFIG"] = str(current_folder) + "/" + selected_kubeconfig
 
 list.clear(arrKubeconfigs)
     
