11b9a7bd81f9e1f89f9980581ed404c18ba1af46
LSWarss <ls.warss98@gmail.com>
2020-09-01 19:03:08 +0200

    Created installation script and it's working for ZSH for now

--- install.py
+++ install.py
@@ -1,17 +1,36 @@
-import pathlib,os,re
+import pathlib,os
 
 current_folder = pathlib.Path(__file__).parent.absolute()
 print(current_folder)
 
 shell = os.environ['SHELL']
+home = os.environ['HOME']
 pattern = "k8set"
 
+print(home)
+
 if("zsh" in shell):
-    file = open("~/.zshrc", "r")
-    for line in file:
-        if(re.search(pattern, line) == True):
-            print("Alias was installed previously, so we will remove old alias.")
+    # file = open("~/.zshrc", "r")
+    # for line in file:
+    #     if(re.search(pattern, line) == True):
+    #         print("Alias was installed previously, so we will remove old alias.")
+    with open(f"{home}/.zshrc", "r") as f:
+        lines = f.readlines()
+    with open(f"{home}/.zshrc", "w") as f:
+        for line in lines:
+            if "k8set" in line:
+                f.write(line)
+        f.truncate()
 
-# TODO: Delete the line where the alias was found
+installation_str =f"alias k8set= python {current_folder}/k8set.py"
 
+print(f"Alias will be installed now by adding {installation_str} to {home}/.zshrc")
 
+if("zsh" in shell):
+    with open (f"{home}/.zshrc", "w") as f:
+        f.write(installation_str)
+        f.close()
+
+print("Alias has been installed.")
+
+print("Installation complete")
