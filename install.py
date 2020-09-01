889eb9649fba090a1890b633542943c494289863
LSWarss <ls.warss98@gmail.com>
2020-09-01 19:28:03 +0200

    Added bash support for installation script Closes #1

--- install.py
+++ install.py
@@ -1,19 +1,12 @@
-import pathlib,os
+import pathlib
+import os
 
 current_folder = pathlib.Path(__file__).parent.absolute()
-print(current_folder)
-
 shell = os.environ['SHELL']
 home = os.environ['HOME']
 pattern = "k8set"
 
-print(home)
-
 if("zsh" in shell):
-    # file = open("~/.zshrc", "r")
-    # for line in file:
-    #     if(re.search(pattern, line) == True):
-    #         print("Alias was installed previously, so we will remove old alias.")
     with open(f"{home}/.zshrc", "r") as f:
         lines = f.readlines()
     with open(f"{home}/.zshrc", "w") as f:
@@ -21,15 +14,30 @@ if("zsh" in shell):
             if "k8set" in line:
                 f.write(line)
         f.truncate()
+elif("bash" in shell):
+    with open(f"{home}/.bash_profile", "r") as f:
+        lines = f.readlines()
+    with open(f"{home}/.bash_profile", "w") as f:
+        for line in lines:
+            if "k8set" in line:
+                f.write(line)
+        f.truncate()
 
 installation_str =f"alias k8set= python {current_folder}/k8set.py"
 
-print(f"Alias will be installed now by adding {installation_str} to {home}/.zshrc")
+
 
 if("zsh" in shell):
+    print(f"Alias will be installed now by adding {installation_str} to {home}/.zshrc")
     with open (f"{home}/.zshrc", "w") as f:
         f.write(installation_str)
         f.close()
+elif("bash" in shell):
+    print(f"Alias will be installed now by adding {installation_str} to {home}/.bash_profile")
+    with open (f"{home}/.bash_profile", "w") as f:
+        f.write(installation_str)
+        f.close()
+
 
 print("Alias has been installed.")
 
