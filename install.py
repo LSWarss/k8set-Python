f75dfd444e93ea706a3ce730cb2589c136cbebd0
LSWarss <ls.warss98@gmail.com>
2020-09-05 18:42:06 +0200

    Added installing the script for bash support

--- install.py
+++ install.py
@@ -15,13 +15,22 @@ if("zsh" in shell):
                 f.write(line)
         f.truncate()
 elif("bash" in shell):
-    with open(f"{home}/.bash_profile", "r") as f:
-        lines = f.readlines()
-    with open(f"{home}/.bash_profile", "w") as f:
-        for line in lines:
-            if "k8set" not in line:
-                f.write(line)
-        f.truncate()
+    if(os.path.isfile(f"{home}/.bash_profile")):
+        with open(f"{home}/.bash_profile", "r") as f:
+            lines = f.readlines()
+        with open(f"{home}/.bash_profile", "w") as f:
+            for line in lines:
+                if "k8set" not in line:
+                    f.write(line)
+            f.truncate()
+    elif(os.path.isfile(f"{home}/.bashrc")):
+        with open(f"{home}/.bashrc", "r") as f:
+            lines = f.readlines()
+        with open(f"{home}/.bashrc", "w") as f:
+            for line in lines:
+                if "k8set" not in line:
+                    f.write(line)
+            f.truncate()
 
 installation_str = f"alias k8set='python {current_folder}/src/k8set.py'"
 
@@ -31,10 +40,16 @@ if("zsh" in shell):
         f.write(installation_str)
         f.close()
 elif("bash" in shell):
-    print(f"Alias will be installed now by adding {installation_str} to {home}/.bash_profile")
-    with open (f"{home}/.bash_profile", "a") as f:
-        f.write(installation_str)
-        f.close()
+    if(os.path.isfile(f"{home}/.bash_profile")):
+        print(f"Alias will be installed now by adding {installation_str} to {home}/.bash_profile")
+        with open (f"{home}/.bash_profile", "a") as f:
+            f.write(installation_str)
+            f.close()
+    elif(os.path.isfile(f"{home}/.bashrc")):
+        print(f"Alias will be installed now by adding {installation_str} to {home}/.bashrc")
+        with open (f"{home}/.bashrc", "a") as f:
+            f.write(installation_str)
+            f.close()
 
 
 print("Alias has been installed.")
